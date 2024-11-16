3eef5b11f7568e92b3af2943bac4cf54e01fd0c6
Yago Aquino <yagopacheco.34@gmail.com>
2024-11-16 12:41:35 -0300

    Adicionando projeto Fast API

---
+++ Fast_api/tests/test_app.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrande (organização)
+    response = client.get('/')  # Act (ação)
+    assert response.status_code == HTTPStatus.OK  # Assert (resultado)
+    assert response.json() == {'message': 'Olá mundo'}  # Assert
